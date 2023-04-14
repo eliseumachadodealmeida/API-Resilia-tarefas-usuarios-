2fa348b3f1dc327f0d62e3d74b464070afaad2ed
Eliseu Machado <81834452+eliseumachadodealmeida@users.noreply.github.com>
2023-04-14 20:16:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 // Importando o packages
 import express from 'express'
+import cors from 'cors'
 
 // instanciando o servidor
 const app = express()
 
 // configurando o servidor para receber requisições com o corpo no formato JSON
 app.use(express.json())
+app.use(cors())
 
 // importando os controllers
 import usuarioController from './controllers/usuarioController.js'
